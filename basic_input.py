384921d6abdad5ffe780cf2b04147b53b2396076
abhishek-rakshe <abhir.31033@gmail.com>
2024-06-12 11:44:12 +0530

    Add files via upload

---
+++ basic_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:- ")
+age = int(input("Enter your age:- "))
+desig = input("Enter your Designation:- ")
+city = input("Enter your City:- ")
+print("Hello, everyone. My Name is", name, "I am",age,"years old. My designation is ", desig, "and i am form",
+      city)
